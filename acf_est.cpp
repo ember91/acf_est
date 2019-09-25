68a162dc62eb2bd28896c3872427eb0e7bc722ac
Emil Berg <emil.berg1991@gmail.com>
2019-09-25 22:22:13 +0200

    Format document

--- acf_est.cpp
+++ acf_est.cpp
@@ -13,7 +13,6 @@
 // Agner Fog's vectorclass library headers
 #include "vectorclass/vectorclass.h"
 
-
 // TODO list:
 // * Add support for floats, ints ...?
 // * Remove vectorclass in favour of VcDevel/Vc?
@@ -22,6 +21,7 @@
 // * Mex ID:s are wrong in mexErrMsgIdAndText
 // * Format code
 // * Add README
+// * Change formatter defaults
 
 /** Parameters to spawned threads */
 struct ThreadParams
@@ -30,8 +30,8 @@ struct ThreadParams
     unsigned index;     /**< Thread index (0 <= index < n_threads) */
     mwSize N;           /**< Number of rows in matrix (Size of each ACF estimation) */
     mwSize C;           /**< Number of columns in matrix (Number of ACFs to estimate) */
-    mxDouble* x;        /**< Input matrix [N, C] */
-    mxDouble* y;        /**< Output matrix [(2*N-1), C] */
+    mxDouble *x;        /**< Input matrix [N, C] */
+    mxDouble *y;        /**< Output matrix [(2*N-1), C] */
 };
 
 /**
@@ -41,7 +41,7 @@ struct ThreadParams
  * \return Nothing
  * 
  */
-void* calculate(const ThreadParams& p)
+void *calculate(const ThreadParams &p)
 {
     // Iterate through each column
     for (mwSize c = 0; c < p.C; ++c)
@@ -49,33 +49,33 @@ void* calculate(const ThreadParams& p)
         // Iterate through each input index
         for (mwSize k = (c + p.index) % p.n_threads; k < p.N; k += p.n_threads)
         {
-            double s = 0.0; /**< Current sum */
-#if 1 // Enable for correct but slow implementation
+            double s = 0.0;                  /**< Current sum */
+#if 1                                        // Enable for correct but slow implementation
             int lim = (int)p.N - (int)k - 3; /**< Iteration limit */
             int n;
             for (n = 0; n < lim; n += 4)
             {
                 // Vectorized multiplication and summation
-                Vec4d v1 = Vec4d().load(p.x + c*p.N + n);
-                Vec4d v2 = Vec4d().load(p.x + c*p.N + n + k);
-                Vec4d prod = v1*v2;
+                Vec4d v1 = Vec4d().load(p.x + c * p.N + n);
+                Vec4d v2 = Vec4d().load(p.x + c * p.N + n + k);
+                Vec4d prod = v1 * v2;
                 s += horizontal_add(prod);
             }
 
             // Don't forget the last elements that didn't fit into a vector
             for (; n < p.N - k; ++n)
-                s += p.x[c*p.N + n]*p.x[c*p.N + n + k];
+                s += p.x[c * p.N + n] * p.x[c * p.N + n + k];
 #else
             for (size_t n = 0; n < N - k; ++n)
-                s += x[n]*x[n + k];
+                s += x[n] * x[n + k];
 #endif
-            
+
             // Divide by N and write twice, due to ACF symmetry
-            double d = s/p.N;
-            p.y[c*p.N + k] = d;
+            double d = s / p.N;
+            p.y[c * p.N + k] = d;
         }
     }
-    
+
     return nullptr;
 }
 
@@ -106,17 +106,17 @@ unsigned detectNumberOfCores()
  * \param vIn Input array
  * \return Output array
  */
-mxArray* spawnThreads(const mxArray* vIn)
+mxArray *spawnThreads(const mxArray *vIn)
 {
     // Get matrix dimensions
-    const mwSize* dims = mxGetDimensions(vIn);
+    const mwSize *dims = mxGetDimensions(vIn);
     mwSize N = dims[0];
     mwSize C = dims[1];
 
     // Create output matrix
-    mxArray* vOut = mxCreateDoubleMatrix(N, C, mxREAL);
+    mxArray *vOut = mxCreateDoubleMatrix(N, C, mxREAL);
 
-    // Ensure that the first non-singular dimension is handled 
+    // Ensure that the first non-singular dimension is handled
     if (N == 1 && C != 1)
         std::swap(C, N);
 
@@ -125,7 +125,7 @@ mxArray* spawnThreads(const mxArray* vIn)
     // Allocate threads and their parameters
     std::vector<std::thread> threads(n_threads);
     std::vector<ThreadParams> params(n_threads);
-    
+
     // Set parameters for each thread
     for (unsigned i = 0; i < n_threads; ++i)
     {
@@ -133,10 +133,10 @@ mxArray* spawnThreads(const mxArray* vIn)
         params[i].index = i;
         params[i].N = N;
         params[i].C = C;
-        params[i].x = (mxDouble*) mxGetData(vIn);
-        params[i].y = (mxDouble*) mxGetData(vOut);
+        params[i].x = (mxDouble *)mxGetData(vIn);
+        params[i].y = (mxDouble *)mxGetData(vOut);
     }
-    
+
     // Start all threads
     for (unsigned i = 0; i < n_threads; ++i)
         threads[i] = std::move(std::thread(calculate, params[i]));
@@ -157,16 +157,16 @@ mxArray* spawnThreads(const mxArray* vIn)
  * \param prhs Right hand parameters [nrhs]
  * 
  */
-void checkArguments(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs)
+void checkArguments(int nlhs, mxArray **plhs, int nrhs, const mxArray **prhs)
 {
     (void)(plhs);
-    
+
     if (nrhs != 1)
         mexErrMsgIdAndTxt("acf_est", "One input required");
 
     if (!mxIsDouble(prhs[0]))
         mexErrMsgIdAndTxt("acf_est", "Input matrix must be of type double");
-    
+
     if (mxGetNumberOfDimensions(prhs[0]) >= 4)
         mexErrMsgIdAndTxt("acf_est", "cannot handle 4-dimensional matrices or greater");
 
@@ -183,10 +183,10 @@ void checkArguments(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs)
  * \param prhs Right hand parameters [nrhs]
  * 
  */
-void mexFunction(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs)
+void mexFunction(int nlhs, mxArray **plhs, int nrhs, const mxArray **prhs)
 {
     checkArguments(nlhs, plhs, nrhs, prhs);
-    mxArray* y = spawnThreads(prhs[0]);
+    mxArray *y = spawnThreads(prhs[0]);
     if (nrhs >= 1)
         plhs[0] = y;
 }
